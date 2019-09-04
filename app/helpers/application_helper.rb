7c05d254b8a6841a1886da77b517c1bdd7175f8f
Zac <zac4793@gmail.com>
2019-09-04 12:50:05 +0100

    Set default page title to 'Microblogger'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # Returns the full title for a page, or the default, if the page has no name
+  def full_title(page_name = '')
+    base_title = 'Microblogger'
+    return base_title if page_name.empty?
+
+    page_name + ' | ' + base_title
+  end
 end
