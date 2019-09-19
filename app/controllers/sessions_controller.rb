67bb569a27b22d1d0be25e3f90621c964c646fcd
Zac <zac4793@gmail.com>
2019-09-19 20:40:50 +0100

    remove unneccessary strong params from session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    session_params = whitelist_session_params
+    session_params = params[:session]
     @user = User.find_by(email: session_params[:email].downcase)
     if @user&.authenticate(session_params[:password])
       accept_user session_params
@@ -14,11 +14,5 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
-  end
-
-  private
-
-  def whitelist_session_params
-    params.require(:session).permit(:email, :password, :remember_me)
   end
 end
