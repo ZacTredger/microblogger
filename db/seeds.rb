1d89da5e839d7c27ec734eb1b395b2daae805105
Zac <zac4793@gmail.com>
2019-10-12 19:19:03 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,30 @@
+def random_short_lorem
+  lorem = Faker::Lorem.sentence(word_count: rand(20))
+  return lorem if lorem.length < 140
+
+  lorem[1..140].sub(/\s\w*$/, '')
+end
+
+password = 'password'
 User.create!(name: 'Example User',
              email: 'example@railstutorial.org',
-             password: 'password',
-             password_confirmation: 'password',
+             password: password,
+             password_confirmation: password,
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
 
 99.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n + 1}@railstutorial.org"
-  password = 'password'
-  attributes = { name: name,
-                 email: email,
-                 password: password,
-                 password_confirmation: password }
-  attributes.merge(activated: true, activated_at: Time.zone.now) unless
-    (n % 13).zero?
-  User.create! attributes
+  activated = !(n % 13).zero?
+  User.create!(name: Faker::Name.name,
+               email: "example-#{n + 1}@railstutorial.org",
+               password: password,
+               password_confirmation: password,
+               activated: activated,
+               activated_at: activated ? Time.zone.now : nil)
+end
+
+users = User.take(5)
+50.times do
+  users.each { |user| user.posts.create!(content: random_short_lorem) }
 end
