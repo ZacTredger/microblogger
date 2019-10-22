75cac71f65b3c53d0d57ab8dcaf6ec57918428ab
Zac <zac4793@gmail.com>
2019-10-22 16:00:44 +0100

    Update seeds helper to fix bug
    Was accidentally using the inclusive range `[0..140]`, which was occasionally, randomly, creating strings of length 141, if they happened to end with a space. This causes post validations to fail during db:seed.

--- db/helpers.rb
+++ db/helpers.rb
@@ -1,9 +1,9 @@
 
 def random_short_lorem
   lorem = Faker::Lorem.sentence(word_count: rand(1..20))
-  return lorem if lorem.length < 140
+  return lorem if lorem.length <= 140
 
-  lorem[0..140].sub(/\s\w*$/, '')
+  lorem[0...139].sub(/\s\w*$/, '.')
 end
 
 def random_created_at
