bfcff55c0f649d73f5ab0a21c89391bed67ad6a5
Zac <zac4793@gmail.com>
2019-11-14 22:01:18 +0000

    Add integration test for feeds

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -56,4 +56,11 @@ class FollowingTest < ActionDispatch::IntegrationTest
       assert_select 'a[href=?]', user_path(user)
     end
   end
+
+  test 'homepage feed' do
+    get root_path
+    @user.feed.paginate(page: 1).each do |post|
+      assert_match CGI.escapeHTML(post.content), response.body
+    end
+  end
 end
